20f029a1baadeecdb5ab397d6a38bc8d03cd4c33
yuswa-arba <yuswa98@gmail.com>
2025-06-04 10:44:40 +0800

    Add private api script

--- pkg/config.go
+++ pkg/config.go
@@ -22,6 +22,9 @@ var (
 	// HostFull --> Host with protocol
 	HostFull string
 
+	// PrivateHost --> Private Host for microservice communication
+	PrivateHost string
+
 	// DevMode --> Dev mode for use .env or kubernetes configmap
 	DevMode bool
 
@@ -60,6 +63,10 @@ func InitHost() {
 	}
 
 	Host += ":" + port
+}
+
+func InitPrivateHost() {
+	PrivateHost = os.Getenv("PRIVATE_HOST")
 }
 
 func InitDevMode() {
